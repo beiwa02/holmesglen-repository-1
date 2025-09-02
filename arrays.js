d789fb207329a8157b71f32db4a14edf4df0ca39
beiwa02 <bailiewalsh1@gmail.com>
2025-09-02 12:15:42 +1000

    Q1.1

---
+++ arrays.js
@@ -0,0 +1,3 @@
+// Q1.1: Create array, log to console
+const unorderedArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20]; // Unordered array of numbers
+console.log(unorderedArray); // Logs above array to the console
