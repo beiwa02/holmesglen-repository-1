9bb768a75bc908ecb1024bfe3b1c48e7027989de
beiwa02 <bailiewalsh1@gmail.com>
2025-09-02 12:18:10 +1000

    Q1.2

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 // Q1.1: Create array, log to console
 const unorderedArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20]; // Unordered array of numbers
 console.log(unorderedArray); // Logs above array to the console
+
+// Q1.2: Sort array in ascending order
+const ascendingArray = unorderedArray.sort(function(a, b){return a - b}); // New array; takes unorderedArray and sorts the numbers in ascending order
+console.log(ascendingArray); // Logs above array to the console
