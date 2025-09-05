5de784eb3c651e6509f20550038da0892ccfe88f
beiwa02 <bailiewalsh1@gmail.com>
2025-09-05 16:23:45 +1000

    Q2.2

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ const myMovie = {
   year: 1964,
 }
 console.log(myMovie);
+
+// Q2.2: Add properties
+myMovie.rating = 5;
+myMovie.summary = "World War II movie about Normandy landings";
+console.log(myMovie);
